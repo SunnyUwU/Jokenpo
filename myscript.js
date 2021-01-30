4e3f96cca54a858b3365b27207aa0cf86c284d5f
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2021-01-30 17:16:24 -0300

    Update myscript.js
    Get elements 'pedra', 'papel' and 'tesoura'

---
+++ myscript.js
@@ -0,0 +1,8 @@
+//Ao clicar em pedra
+document.getElementById('pedra').onclick = function() {};
+
+//Ao clicar em papel
+document.getElementById('papel').onclick = function() {};
+
+//Ao clicar em tesoura
+document.getElementById('tesoura').onclick = function() {};
