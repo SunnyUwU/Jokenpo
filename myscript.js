3ba1a131c8425f4ef712f0896d3617a3c71dc3f5
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2021-01-30 17:18:40 -0300

    Update myscript.js
    Ask the player's name

--- myscript.js
+++ myscript.js
@@ -6,3 +6,7 @@ document.getElementById('papel').onclick = function() {};
 
 //Ao clicar em tesoura
 document.getElementById('tesoura').onclick = function() {};
+
+//Informar o nome do jogador
+var jogadorNome;
+jogadorNome = prompt('Qual é o seu nome?');
