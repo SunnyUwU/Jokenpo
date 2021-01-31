c0ac663f698256e3faf3b09be53c35695c056673
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2021-01-31 13:53:38 -0300

    Create script.js
    Create script.js and get elements "pedra", "papel" and "tesoura"

---
+++ script.js
@@ -0,0 +1,15 @@
+//Ao clicar em pedra
+document.getElementById('jogador-escolha-1').onclick = function() {
+    console.log("pedra")
+};
+
+//Ao clicar em papel
+document.getElementById('jogador-escolha-2').onclick = function() {
+    console.log("papel");
+};
+
+//Ao clicar em tesoura
+document.getElementById('jogador-escolha-3').onclick = function() {
+    console.log("tesoura");
+};
+
